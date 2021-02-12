2f303c8c28cf4ff9236b8701434607155ebaea93
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-02-12 22:15:06 +0900

    programmers Level1 #003

---
+++ Programmers_CodingTest/003_두정수사이의합.js
@@ -0,0 +1,15 @@
+function solution(a, b) {
+    var answer = 0;
+    
+    if(a<=b){
+      for(let i = a ; i <=b ; i++ ){
+        answer = answer + i
+      }    
+    }
+    if(a>b){
+        for(let i = b; i <=a ; i++){
+            answer =answer+i
+        }
+    }
+    return answer;
+}

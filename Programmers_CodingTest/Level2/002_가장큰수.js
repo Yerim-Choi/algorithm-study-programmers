935186617fa21270373ee9a4f72a13ad15f76462
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-02-28 21:49:28 +0900

    programmers Level2 #002

---
+++ Programmers_CodingTest/Level2/002_가장큰수.js
@@ -0,0 +1,7 @@
+function solution(numbers) {
+    let answer = numbers
+      .map((v) => v + "")
+      .sort((a, b) => (b + a) * 1 - (a + b) * 1)
+      .join("");
+    return answer[0] === "0" ? "0" : answer;
+  }

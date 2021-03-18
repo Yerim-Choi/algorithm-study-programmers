9e4e3884e2c3c1bd04273d93dda25874bd295320
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-03-18 21:43:04 +0900

    programmers Level2 #010 ver2

--- Programmers_CodingTest/Level2/10_문자열압축.js
+++ Programmers_CodingTest/Level2/10_문자열압축.js
@@ -1,42 +1,17 @@
-function solution(s) {
-    let arr = [];
-    // 문자열 크기의 절반 수까지 i개 단위로 압축
-    for(let i=1; i<= parseInt(s.length/2); i++){
-        // 0부터 i개의 문자가 첫 번째 압축 문자
-        let str = s.substr(0,i);
-        let cnt = 1;
-        // j-i부터 i개의 문자와 j부터 i개의 문자가 같으면 cnt 1증가
-        // 아니면 str에 그 문자를 숫자와 그대로 붙이기 (1이면 문자만 붙임)
-        // 이때 j는 i만큼씩 증가
-        for (let j=i; j<= s.length-i; j=j+i){
-            if (s.substr(j-i,i) === s.substr(j,i)){
-                cnt++;
-                if(s.length-j-i<i){
-                    str+= cnt;
-                }
-            }else{
-                if (cnt !== 1){
-                    str+= cnt;
-                    str+=s.substr(j,i);
-                }else{
-                    str+=s.substr(j,i);
-                }
-                cnt = 1;
-            }
-        }
-        // 남은 문자열은 압축할 수 없으므로 그대로 붙이기
-        str+=s.substr(s.length-1-(s.length % i), (s.length % i));
-        arr.push(str);
-    }
-    // 제일 압축 잘한 경우 반환
-    let min = 100000;
-    for (let i=0; i< arr.length; i++){
-        if (arr[i].length < min){
-            min = arr[i].length;
-        }
-    }
-    if (s.length === 1)
-        min = 1;
-    console.log(arr);
-    return min;
-}
+const solution = s => {
+    const range = [...Array(s.length)].map((_, i) => i + 1);
+    return Math.min(...range.map(i => compress(s, i).length));
+  };
+  
+  const compress = (s, n) => {
+    const make = ([a, l, c]) => `${a}${c > 1 ? c : ''}${l}`;
+    return make(
+      chunk(s, n).reduce(
+        ([a, l, c], e) => e === l ? [a, l, c + 1] : [make([a, l, c]), e, 1],
+        ['', '', 0]
+      )
+    );
+  };
+  
+  const chunk = (s, n) =>
+    s.length <= n ? [s] : [s.slice(0, n), ...chunk(s.slice(n), n)];

87310f8215542d3ac1ad8e1a540e444f2e601ae7
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-04-01 20:07:02 +0900

    programmers Level2 #014

---
+++ Programmers_CodingTest/Level2/014_짝지어제거하기.js
@@ -0,0 +1,12 @@
+function solution(s)
+{
+    var stringToArray = s.split('');
+    var res = [];
+
+    for (var val of stringToArray){
+        if( val === res[res.length -1]){
+            res.pop() 
+        } else { res.push(val)}
+    } 
+    return res.length === 0 ? 1 : 0 ;
+}
